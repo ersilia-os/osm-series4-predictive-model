96e972024275c63de43bd3179f0f9d625b61f9e1
miquelduranfrigola <miquelduranfrigola@gmail.com>
2021-07-08 12:06:29 +0200

    gemma

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import joblib
 import pandas as pd
 import numpy as np
 import onnxruntime as rt
-import mols2grid
+import json
+import os
 
 from rdkit import Chem
 from rdkit.Chem import AllChem
@@ -14,43 +15,82 @@ from rdkit.Chem import Draw
 from rdkit.DataStructs import BulkTanimotoSimilarity
 
 from src.fingerprinter import get_fingerprints, mols_to_fingerprints, ra_fingerprint
+from src.eosdescriptors.chembl import Chembl
+from src.eosdescriptors.ecfp import Ecfp
+from src.eosdescriptors.rdkit2d import Rdkit2d
+
+
+# APP:
 
 st.title("Properties calculation for OSM Series 4 molecules")
 
+# First section: molecule input and drawing
 col1, col2 = st.beta_columns(2)
-
-col1.subheader("Input molecule in smiles format")
+#Input
+col1.subheader("Input molecule in SMILES format")
 smiles = col1.text_input("", value = "")
 if not smiles:
     mol = None
 else:
     mol=Chem.MolFromSmiles(smiles)
-
+#Image
 col2.subheader("Molecule display")
 if mol is not None:
     col2.image(Draw.MolToImage(mol), width=200)
 
-#get series4 molecules for tanimoto similarity
-df = pd.read_csv("data/series4_processed.csv")
-s4_smiles = df["smiles"].tolist()
-s4_mols=[Chem.MolFromSmiles(smi) for smi in s4_smiles]
-ref_fps=mols_to_fingerprints(s4_mols)
-
-X = get_fingerprints([smiles])
-
+# Second section: prediction of properties
 st.header("Properties")
-col1, col2, col3, col4, col5, col6 =st.beta_columns(6) 
+col1, col2, col3, col4, col5, col6 =st.beta_columns(6)
 
 col1.subheader("Activity")
-def load_model(path):
-    mdl = joblib.load(path)
-    return mdl    
+MODELS_DIR = "model"
+
+models_files = {}
+with open(os.path.join(MODELS_DIR, "models.json"), "r") as f:
+    all_models = json.load(f)
+for dn, tn in all_models:
+    models_files[(dn, tn)] = os.path.join(MODELS_DIR, "{0}_{1}.pkl".format(dn, tn))
+
+sessions = {}
+for k, v in models_files.items():
+    sess = joblib.load(v)
+    sessions[k] = sess
+
+necessary_descriptors = sorted(set([k[0] for k in all_models]))
+necessary_tasks = sorted(set([k[1] for k in all_models]))
+
+descriptor_calculators = {
+    "chembl": Chembl(),
+    "ecfp": Ecfp(),
+    "rdkit2d": Rdkit2d()
+}
+
+def one_prediction(mol):
+    preds = {}
+    for dn in necessary_descriptors:
+        fp = descriptor_calculators[dn]
+        X = fp.calc([mol])
+        for tn in necessary_tasks:
+            sess = sessions[(dn, tn)]
+            if tn == "classification":
+                pred = sess.predict_proba(X)[:,1]
+            else:
+                pred = sess.predict(X)[:]
+            preds[(dn, tn)] = pred[0]
+    return preds
+
+
 if mol is None:
     y=0
+    stdev=0
 else:
-    mdl = load_model("model/my_first_model.pkl")
-    y = mdl.predict(X)[0]
-col1.write("{0:.2f}".format(y), help="Results interpretation: 0 = no activity - 1 = maximum activity")
+    preds=one_prediction(mol)
+    values=[]
+    for k,v in preds.items():
+        values += [v]
+    y=np.mean(values)
+    stdev=np.std(values)
+col1.write("{0:.2f} +/- {1:.2f}".format(y,stdev))
 
 col2.subheader("MW")
 if mol is None:
@@ -59,7 +99,7 @@ else:
     mw=ExactMolWt(mol)
 col2.write("{0:.2f}".format(mw))
 
-col3.subheader("Solubility")
+col3.subheader("SLogP")
 if mol is None:
     logp = 0
 else:
@@ -74,6 +114,10 @@ else:
 col4.write("{0:.2f}".format(qed_score))
 
 col5.subheader("S4 Sim")
+df = pd.read_csv("data/series4_processed.csv") #get series4 molecules for tanimoto similarity
+s4_smiles = df["smiles"].tolist()
+s4_mols=[Chem.MolFromSmiles(smi) for smi in s4_smiles]
+ref_fps=mols_to_fingerprints(s4_mols)
 if mol is None:
     tan=0
 else:
@@ -92,13 +136,29 @@ else:
     label_name = sess.get_outputs()[1].name
     ra = sess.run([label_name], {input_name: ra_fp})[0][0][1]
 col6.write("{0:.2f}".format(ra))
-    
-               
+
+# Third section: individual activity predictions
+
+if mol is not None:
+    st.header("Activity Predictions")
+    col1, col2, col3, col4, col5, col6 =st.beta_columns(6)
+    the_cols = [col1, col2, col3, col4, col5, col6]
+
+    i = 0
+    for task in ["classification", "regression"]:
+       for n, desc in enumerate(["ecfp", "rdkit2d", "chembl"]):
+           my_col = the_cols[i]
+           pred = preds[(desc, task)]
+           my_col.subheader("{0}{1}".format(task[0].upper(), n+1))
+           my_col.write("{0:.2f}".format(pred))
+           i += 1
+
+#Fourth section: details on each prediction
 st.header("Property details")
-        
+
 #activity
 st.subheader("Activity prediction")
-st.write("Activity against the malaria parasite is predicted using the ML model [eoschem](https://github.com/ersilia-os/ersilia-automl-chem). This model has been trained using the dataset provided by the [Open Source Malaria Project](https://github.com/opensourcemalaria) and only takes into account series 4 molecules. Thus, it will be most accurate for molecules from the same series.")
+st.write("Average score across all predictors (3 classifiers and 3 regressors) +/- standard deviation. Individual scores are detailed in the Activity Predictions row")
 st.text("Interpretation: 0 = no activity - 1 = maximum activity")
 
 #Molecular weight
@@ -125,4 +185,3 @@ st.text("Interpretation: 0 = completely different - 1 = very similar to existing
 st.subheader("Retrosynthetic Accessibility (RA) score")
 st.write("Estimation of the synthetic feasibility for the input molecule as determined from the predictions of the computer aided synthesis planning tool AiZynthFinder. Developed by the [Reymond Group](https://github.com/reymond-group/RAscore)")
 st.text("Interpretation: 0 = no synthetic route identified - 1 = synthetic route available")
-
